e9bd906e064280a884e2464a02e0ef8ce37f877c
Ben Bingham <ben@benbingham.ca>
2026-02-20 16:43:27 -0700

    Add some server testing

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -8,12 +8,20 @@ def server_response():
     code = request.args.get("code", "")
     return f"""
     <html>
-      <body>
-        <h1>Received JavaScript Code</h1>
-        <pre>{escape(code)}</pre>
-      </body>
+        <body>
+            <h1>Received JavaScript Code</h1>
+            <pre>{escape(code)}</pre>
+        </body>
     </html>
     """
+
+@app.route("/api/v1/task", methods = ["POST"])
+def postRequest():
+    print(request.json)
+
+	# COMPUTE
+
+    return jsonify({"Result":4})
 
 if __name__ == "__main__":
     app.run(debug=True)
