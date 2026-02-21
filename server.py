b7be0172b61bc2ac6fc0b929e32e01800482f480
Ben Bingham <ben@benbingham.ca>
2026-02-20 17:25:37 -0700

    Added executing posted code

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from markupsafe import escape
+import subprocess
 
 app = Flask(__name__)
 
@@ -15,11 +16,30 @@ def server_response():
     </html>
     """
 
+class Job:
+	jsFunction = ""
+	args = []
+
+def distribute(job):
+    docName = "func.txt"
+    with open(docName, "w") as text_file:
+        text_file.write(job.jsFunction)
+    
+    with open(docName, "r") as text_file:
+        print(text_file.read())
+
+    for arg in job.args:
+        subprocess.run(["mujs", docName])
+
 @app.route("/api/v1/task", methods = ["POST"])
 def postRequest():
     print(request.json)
 
-	# COMPUTE
+    newJob = Job()
+    newJob.jsFunction = request.json["func"]
+    newJob.args = request.json["args"]
+
+    distribute(newJob)
 
     return jsonify({"Result":4})
 
--- testServer.py
+++ testServer.py
@@ -1,4 +1,4 @@
 import requests
-res = requests.post("http://localhost:5000/api/v1/task", json={"func":"test", "args":[1, 2, 3, 4]})
+res = requests.post("http://localhost:5000/api/v1/task", json={"func":"console.log(\"test_print_message\");", "args":[1, 2, 3, 4]})
 if res.ok:
     print(res.json())
